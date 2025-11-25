0f2400156bd7a145acaf37d63a8af208da7e5bee
Crissilva1 <cristianedasilva1267@gmail.com>
2025-11-25 19:54:53 +0000

    começando agenda em sala

---
+++ tasks/poo/agenda/py/draft.py
@@ -0,0 +1,27 @@
+class Fone:
+    def __init__(self, id:str, number:str):
+        self.__id :str = id
+        self.__number:str = number
+
+    def get_number(self)->str:
+        return self.__number
+    
+    def get_id(self)->str:
+        return self.__id
+    
+    def __str__(self)->str:
+        return f"{self.__number}:{self.__id}"
+    
+class Contato:
+    def __init__(self, nome:str, favorito:bool):
+        self.__nome:str = nome
+        self.__favorito:bool = favorito
+        self.__fone:list[Fone|None]=[]
+    
+    def get_nome(self)->str:
+        return self.__nome
+    
+    def get_favorito(self)->bool:
+        return self.__favorito
+    
+    def get_
